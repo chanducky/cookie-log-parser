ca5bf01955e513ee086c516b389bbdaf52d12297
chanducky <chandu.cky@gmail.com>
2022-07-18 09:47:50 +0530

    updated integration tests.

--- src/test/java/com/qc/cookielogparser/AppIntegrationTest.java
+++ src/test/java/com/qc/cookielogparser/AppIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -27,36 +28,63 @@ class AppIntegrationTest
     @Autowired
     private CookieLogService cookieLogService;
 
-    @Test
-    void testCall()
-            throws IOException, ParseException
+    private List<CookieDetail> cookieLogs;
+
+    @PostConstruct
+    void setUp()
+            throws IOException
     {
         URL url = this.getClass().getResource("/csv/cookie_log.csv");
-        List<CookieDetail> cookieLogs = fileParserService.parseCsvToBean(new File(url.getFile()),
-                                                                         CookieDetail.class);
+        cookieLogs = fileParserService.parseCsvToBean(new File(url.getFile()),
+                                                      CookieDetail.class);
+    }
 
+    @Test
+    void testNoMatchOnDate()
+            throws ParseException
+    {
         Set<CookieDetail> mostActiveCookiesOn10ThDec = cookieLogService
                 .searchMostActiveCookiesForDate(cookieLogs, AppConstants.SDF_DATE.parse("2018-12-10"));
         Assertions.assertNull(mostActiveCookiesOn10ThDec);
+    }
 
+    @Test
+    void testMultipleMatchByDateButSingleMostActiveAtCorner()
+            throws ParseException
+    {
         Set<CookieDetail> mostActiveCookiesOn9ThDec = cookieLogService
                 .searchMostActiveCookiesForDate(cookieLogs, AppConstants.SDF_DATE.parse("2018-12-09"));
         Assertions.assertNotNull(mostActiveCookiesOn9ThDec);
         Assertions.assertEquals(1, mostActiveCookiesOn9ThDec.size());
         Assertions.assertEquals("AtY0laUfhglK3lC7", mostActiveCookiesOn9ThDec.stream().findFirst().get().getCookie());
+    }
 
+    @Test
+    void testMultipleMatchByDateButSingleMostActiveInMiddle()
+            throws ParseException
+    {
         Set<CookieDetail> mostActiveCookiesOn8ThDec = cookieLogService
                 .searchMostActiveCookiesForDate(cookieLogs, AppConstants.SDF_DATE.parse("2018-12-08"));
         Assertions.assertNotNull(mostActiveCookiesOn8ThDec);
         Assertions.assertEquals(1, mostActiveCookiesOn8ThDec.size());
         Assertions.assertEquals("SAZuXPGUrfbcn5UA", mostActiveCookiesOn8ThDec.stream().findFirst().get().getCookie());
+    }
 
+    @Test
+    void testSingleMatchByDateAndSingleMostActive()
+            throws ParseException
+    {
         Set<CookieDetail> mostActiveCookiesOn7ThDec = cookieLogService
                 .searchMostActiveCookiesForDate(cookieLogs, AppConstants.SDF_DATE.parse("2018-12-07"));
         Assertions.assertNotNull(mostActiveCookiesOn7ThDec);
         Assertions.assertEquals(1, mostActiveCookiesOn7ThDec.size());
         Assertions.assertEquals("4sMM2LxV07bPJzwf", mostActiveCookiesOn7ThDec.stream().findFirst().get().getCookie());
+    }
 
+    @Test
+    void testMultipleMatchByDateMultipleMostActive()
+            throws ParseException
+    {
         Set<CookieDetail> mostActiveCookiesOn5ThJuly = cookieLogService
                 .searchMostActiveCookiesForDate(cookieLogs, AppConstants.SDF_DATE.parse("2018-07-05"));
         Assertions.assertNotNull(mostActiveCookiesOn5ThJuly);
